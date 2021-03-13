370737f12307d7f1fcfa7f777ac125f1701f87d4
Alonzo <zcodes101@gmail.com>
2021-03-12 20:30:57 -0500

    connect server side and dialogflow

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send({'hello': 'there'})
-});
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+
+require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
